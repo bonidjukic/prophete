1fa212a8db0e8aebaa90a14a0995f8568ded9847
bonidjukic <boni@neutrinodev.com>
2017-03-14 18:16:28 +0100

    Add simple bootstrap theme frontend layout

--- prophete/prophete/settings.example.py
+++ prophete/prophete/settings.example.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
--- prophete/prophete/urls.py
+++ prophete/prophete/urls.py
@@ -1,10 +1,12 @@
 # prophete URL Configuration
 
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from core.views import HomeView
 
 urlpatterns = [
   url(r'^$', HomeView.as_view(), name='home'),
   url(r'^admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
