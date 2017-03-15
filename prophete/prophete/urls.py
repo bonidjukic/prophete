ea9bb990ed556894324fed5b6437c4650be21524
bonidjukic <boni@neutrinodev.com>
2017-03-15 14:59:43 +0100

    Format urls.py and add MEDIA urls

--- prophete/prophete/urls.py
+++ prophete/prophete/urls.py
@@ -7,7 +7,19 @@ from django.contrib import admin
 from core.views import HomeView, PredictionView
 
 urlpatterns = [
-  url(r'^$', HomeView.as_view(), name='home'),
-  url(r'^data/(?P<prediction_pk>\d+)/$$', PredictionView.as_view(), name='prediction_view'),
-  url(r'^admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  url(
+    r'^$',
+    HomeView.as_view(),
+    name='home'
+  ),
+  url(
+    r'^data/(?P<prediction_pk>\d+)/$$',
+    PredictionView.as_view(),
+    name='prediction_view'
+  ),
+  url(
+    r'^admin/',
+    admin.site.urls
+  ),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
